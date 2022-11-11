af9009102500ef82be16ca8b72d825f4bff3c56b
hrdakr <harada.a.ag@gmail.com>
2022-11-11 20:50:55 +0900

    I solved chap2_q11.

--- chap2/c2_q11.py
+++ chap2/c2_q11.py
@@ -1,1 +1,12 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
+
+# with open('popular-names_notab.txt', 'w') as g:
+#     with open('popular-names.txt','r') as f:
+#         s = f.readlines()
+#         for n in range(len(s)):
+#             g.write(s[n].replace('\t',' ') )
+
+with open('popular-names_notab.txt', 'w') as g:
+    with open('popular-names.txt','r') as f:
+        for row in f:
+            g.write( row.replace('\t',' '))
