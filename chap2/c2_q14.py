bdde4038fd6daef886f60db847754bcdf0d86e11
hrdakr <harada.a.ag@gmail.com>
2022-11-11 22:17:04 +0900

    i solved c2_q14

--- chap2/c2_q14.py
+++ chap2/c2_q14.py
@@ -1,13 +1,12 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
 
 N =int( input())
-x = 0
+
 with open('A14.txt', 'w') as g:
     with open('popular-names.txt','r') as f:
-        for row in f:
-            if x <= N:
+        for i, row in enumerate(f):
+            if i <= N:
                 g.write(row)
-                x += 1
             else :
                 break
 
