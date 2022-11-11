81e584aba8698572dcf20f0471bff8bc45c73d91
hrdakr <harada.a.ag@gmail.com>
2022-11-11 20:12:13 +0900

    I solved chap2_10.

--- chap2/c2_q10.py
+++ chap2/c2_q10.py
@@ -1,1 +1,13 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
+"""
+with open('popular-names.txt','r') as f:
+    #for row in f:
+    #    print (row.strip())
+    print(len(f.readlines()))
+"""
+
+x=0
+with open('popular-names.txt','r') as f:
+    for _ in f:
+        x += 1
+print(x)
