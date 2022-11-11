5283b7fac9fb979fe4966c24a77f1ec88e83c3f3
hrdakr <harada.a.ag@gmail.com>
2022-11-11 22:30:42 +0900

    i solved c2_q15

--- chap2/c2_q15.py
+++ chap2/c2_q15.py
@@ -1,1 +1,15 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
+
+N =int( input())
+
+x=0
+with open('popular-names.txt','r') as f:
+    for _ in f:
+        x += 1
+
+with open('A15.txt', 'w') as g:
+    with open('popular-names.txt','r') as f:
+        for i, row in enumerate(f):
+            if i < x - N:
+                continue 
+            g.write(row)
