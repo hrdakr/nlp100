aad850132f29b87ef2531743be1cb2b171c634a8
hrdakr <harada.a.ag@gmail.com>
2022-11-11 22:09:59 +0900

    c2_q14

--- chap2/c2_q14.py
+++ chap2/c2_q14.py
@@ -1,1 +1,13 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
+
+N = 20
+x = 0
+with open('A14.txt', 'w') as g:
+    with open('popular-names.txt','r') as f:
+        for row in f:
+            if x <= N:
+                g.write(row)
+                x += 1
+            else :
+                break
+
