47e421d333a71035f4dc373e79be927faf23070a
hrdakr <harada.a.ag@gmail.com>
2022-11-11 14:13:47 +0900

    I solved chap1_07

--- chap1/c1_q07.py
+++ chap1/c1_q07.py
@@ -1,3 +1,9 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch01.html 
 
 print("A.07")
+
+x = input("何時の？")
+y = input("何が？")
+z = input("何なの？")
+
+print( '%s 時の %s は %s ' % ( x, y, z ) )
