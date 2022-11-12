e8cd9f6efa8806b7ce1807e666d1565ef9667a52
hrdakr <harada.a.ag@gmail.com>
2022-11-13 08:22:38 +0900

    i solved c3_q20. but I can't save out-put as .txt.

--- chap3/c3_q20.py
+++ chap3/c3_q20.py
@@ -1,1 +1,13 @@
-#I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch03.html 
+#I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
+#ref https://amaru-ai.com/entry/2022/10/06/084919
+import json 
+
+#with open('A20j.json', 'r', encoding="utf-8") as g:
+with open('jawiki-country.json', 'r', encoding="utf-8_sig") as f:
+    for row in f:
+        line = json.loads(row)
+        if line['title'] == 'イギリス':
+            text_uk = line['text']
+        else:
+            continue
+print(text_uk)
--- chap3/c3_q24.py
+++ chap3/c3_q24.py
@@ -1,1 +1,1 @@
-#I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch03.html 
+#I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
