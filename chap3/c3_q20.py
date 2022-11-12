af352b9f7f6dde3b96e517db69c41f32cc200ed6
hrdakr <harada.a.ag@gmail.com>
2022-11-13 08:29:28 +0900

    i searched using panda ver..

--- chap3/c3_q20.py
+++ chap3/c3_q20.py
@@ -1,13 +1,22 @@
 #I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
 #ref https://amaru-ai.com/entry/2022/10/06/084919
-import json 
 
-#with open('A20j.json', 'r', encoding="utf-8") as g:
-with open('jawiki-country.json', 'r', encoding="utf-8_sig") as f:
-    for row in f:
-        line = json.loads(row)
-        if line['title'] == 'イギリス':
-            text_uk = line['text']
-        else:
-            continue
-print(text_uk)
+# import json 
+
+# with open('jawiki-country.json', 'r', encoding="utf-8_sig") as f:
+#     for row in f:
+#         line = json.loads(row)
+#         if line['title'] == 'イギリス':
+#             text_uk = line['text']
+#         else:
+#             continue
+# print(text_uk)
+
+
+#ref2 https://kakedashi-engineer.appspot.com/2020/05/09/nlp100-ch3/
+#by using panda
+
+import pandas as pd
+wiki = pd.read_json('jawiki-country.json', lines = True)
+uk = wiki[wiki['title']=='イギリス'].text.values
+print (uk)
