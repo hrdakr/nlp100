1a121f1fedf7d98c64b1bb6531b4ee2bba2489d2
hrdakr <harada.a.ag@gmail.com>
2022-11-12 16:12:56 +0900

    I solved c2_q17

--- chap2/c2_q17.py
+++ chap2/c2_q17.py
@@ -1,1 +1,9 @@
 #I solved the chapter1 of nlp100 in https://nlp100.github.io/ja/ch02.html 
+
+x = []
+with open('col1.txt','r') as f:
+    for row in f:
+        x += [row.split('\n')[0]]
+    
+names =sorted( set(x))
+print(names)
