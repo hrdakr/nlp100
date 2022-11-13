6ed782260af0abfafdf2931c1519cdd0bd6c74f1
hrdakr <harada.a.ag@gmail.com>
2022-11-13 11:22:50 +0900

    solved c3_q21

--- chap3/c3_q21.py
+++ chap3/c3_q21.py
@@ -1,1 +1,10 @@
 #I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
+
+import re
+
+
+with open('A20.txt', 'r', encoding='utf-8') as f:
+    for row in f:
+        line = row.strip()
+        if '[Category' in line :
+            print(line)
