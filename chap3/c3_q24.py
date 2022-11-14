c656e98ba9891f14e2546935ce71973c0f47b51f
hrdakr <harada.a.ag@gmail.com>
2022-11-14 13:28:08 +0900

    24 solve , 25 not solve yet.

--- chap3/c3_q24.py
+++ chap3/c3_q24.py
@@ -1,2 +1,23 @@
 #I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
 
+import re
+import json
+
+output =''
+with open('A24.txt', 'w', encoding='utf-8') as g:
+    with open('A20.txt', 'r', encoding='utf-8') as f:
+        for row in f:
+            line = row.strip()
+            word =  re.findall(r'\[\[ファイル:(.+?)\|', line)
+            if word:
+                output += word[0] + '\n'
+    g.write(output.strip())
+
+
+# with open('jawiki-country.json', 'r', encoding="utf-8") as f:
+#     for row in f:
+#         line = json.loads(row)
+#         if line['title'] == 'イギリス':
+#             text_uk = line['text']
+
+# print(text_uk[0:10])
--- chap3/c3_q25.py
+++ chap3/c3_q25.py
@@ -1,1 +1,34 @@
 #I solved the chapter3 of nlp100 in https://nlp100.github.io/ja/ch03.html 
+
+import json
+import re
+
+# output =''
+# with open('A24.txt', 'w', encoding='utf-8') as g:
+#     with open('A20.txt', 'r', encoding='utf-8') as f:
+#         for row in f:
+#             line = row.strip()
+#             word =  re.findall(r'\[\[ファイル:(.+?)\|', line)
+#             if word:
+#                 output += word[0] + '\n'
+#     g.write(output.strip())
+
+
+##ref https://amaru-ai.com/entry/2022/10/06/084919
+
+with open('jawiki-country.json', 'r', encoding="utf-8") as f:
+    for row in f:
+        line = json.loads(row)
+        if line['title'] == 'イギリス':
+            text_uk = line['text']
+
+# テンプレートの抽出
+pattern = r'^\{\{基礎情報.*?$(.*?)^\}\}'
+template = re.findall(pattern, text_uk, re.MULTILINE + re.DOTALL)
+print(template)
+
+# フィールド名と値を辞書オブジェクトに格納
+# pattern = r'^\|(.+?)\s*=\s*(.+?)(?:(?=\n\|)|(?=\n$))'
+# result = dict(re.findall(pattern, template[0], re.MULTILINE + re.DOTALL))
+# for k, v in result.items():
+#   print(k + ': ' + v)
